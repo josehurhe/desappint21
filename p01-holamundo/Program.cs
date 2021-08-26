e4bb116f1abc361ac79062a917bcff4559a54f41
josehurhe <jose.hurhe@gmail.com>
2021-08-26 01:52:24 -0500

    Programa Hola Mundo

---
+++ p01-holamundo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace p01_holamundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Mundo!");
+            Console.WriteLine("Dame tu nombre?");
+            string nombre = Console.ReadLine();
+            Console.WriteLine($"{nombre} bienvenido a C#\n");
+            DateTime fecha = DateTime.Now;
+            Console.WriteLine($"Hoy es {fecha:d} y son las {fecha:t}");
+            Console.WriteLine("\nHasta Pronto");
+        }
+    }
+}
