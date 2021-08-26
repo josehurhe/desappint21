17fa0193897ed0e74efebd40aaf088b64aab8bfb
josehurhe <jose.hurhe@gmail.com>
2021-08-26 02:10:42 -0500

    Programa Area Circulo

---
+++ p02-areacirculo/Program.cs
@@ -0,0 +1,18 @@
+﻿// Calcular el area de un circulo
+
+using System;
+
+namespace p02_areacirculo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calculando el area de un circulo");
+            Console.WriteLine("Dame el radio?");
+            float radio = float.Parse(Console.ReadLine());
+            double area = Math.PI * Math.Pow(radio,2);
+            Console.WriteLine($"El area del circulo es {area}");
+        }
+    }
+}
