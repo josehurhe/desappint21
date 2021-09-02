e4c861ef45daeb23f4c387125b2c902e040a9d04
josehurhe <jose.hurhe@gmail.com>
2021-09-02 10:58:27 -0500

    Programa Vector Cubo

---
+++ p10-vectorcubo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace p10_vectorcubo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int TAM = 20;
+            double[] A = new double[TAM];
+            double[] B = new double[TAM];
+            Random random = new Random();
+            for (int i = 0; i < A.Length; i++)
+            {
+                A[i] = random.Next(1,100);
+                B[i] = Math.Pow(A[i],3);
+            }
+            Console.WriteLine("\nElementos de A:"); imprime(A);
+            Console.WriteLine("\nElementos de B:"); imprime(B);
+        }
+        static void imprime(double[] v){
+            for (int i = 0; i < v.Length; i++)
+            {
+                Console.Write($"{v[i]} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
