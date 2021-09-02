a34863002ceb59aa6fb17e678caabe2357ac7fef
josehurhe <jose.hurhe@gmail.com>
2021-09-02 10:31:45 -0500

    Programa Vector Promedio

---
+++ p08-vectorpromedio/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace p08_vectorpromedio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] vector = {10,20,30,40,50,60,70,80,90,100,
+            10,20,30,40,50,60,70,80,90,100,
+            10,20,30,40,50,60,70,80,90,100,
+            10,20,30,40,50,60,70,80,90,100,
+            10,20,30,40,50,60,70,80,90,100};
+            int suma = 0, nmp = 0;
+            float promedio = 0.0f;
+            for (int i = 0; i < vector.Length; i++)
+            {
+                Console.Write($"{vector[i]} ");
+                suma += vector[i];
+            }
+            promedio = suma / vector.Length;
+            Console.WriteLine($"\nLa suma es{suma} y el promedio es {promedio}");
+            for (int i = 0; i < vector.Length; i++)
+            {
+                if (vector[i] > promedio){
+                    Console.Write($"{vector[i]} ");
+                    nmp++;
+                }
+            }
+            Console.WriteLine($"\nTotal de numeros mayores que el promedio {nmp}");
+        }
+    }
+}
