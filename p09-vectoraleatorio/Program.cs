084ae3295cefc007b89d181d6b089e7587d165f5
josehurhe <jose.hurhe@gmail.com>
2021-09-02 10:45:05 -0500

    Programa Vector Aleatorio

---
+++ p09-vectoraleatorio/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace p09_vectoraleatorio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int TAM = 15;
+            int[] A = new int[TAM];
+            int[] B = new int[TAM];
+            int[] C = new int[TAM];
+            Random random = new Random();
+            for (int i = 0; i < A.Length; i++)
+            {
+                A[i] = random.Next(1,100);
+                B[i] = random.Next(1,100);
+                C[i] = A[i] + B[i];
+            }
+            Console.WriteLine("\nElementos de A:"); imprime(A);
+            Console.WriteLine("\nElementos de B:"); imprime(B);
+            Console.WriteLine("\nElementos de C:"); imprime(C);
+        }
+
+        static void imprime(int[] v){
+            for (int i = 0; i < v.Length; i++)
+            {
+                Console.Write($"{v[i]} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
