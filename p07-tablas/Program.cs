21dbddb4e8b0c37487b3c924e4493a989fc1009c
josehurhe <jose.hurhe@gmail.com>
2021-09-03 13:31:48 -0500

    Programa Tablas

---
+++ p07-tablas/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace p07_tablas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = 5;
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine($"{numero} x {i} = {numero*i}");
+            }
+            imprimir(1,100);
+            imprimir(10,10);
+            imprimir(5,11);
+        }
+
+        static void imprimir(int tabla, int numero){
+            for (int i = 1; i <= numero; i++)
+            {
+                Console.WriteLine($"{tabla} x {i} = {tabla*i}");
+            }
+            Console.WriteLine();
+        }
+    }
+}
